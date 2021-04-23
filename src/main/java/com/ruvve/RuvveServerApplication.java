0f54b757e9f0e35f15fe1e11dd9e6f602c808b5a
gngsn <rudtjs4540@gmail.com>
2021-04-23 23:16:25 +0900

    update: separate config

--- src/main/java/com/ruvve/RuvveServerApplication.java
+++ src/main/java/com/ruvve/RuvveServerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RuvveServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(RuvveServerApplication.class, args);
     }
-
 }

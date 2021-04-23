55fee927faa80839de3cbba37072c80ebe613d3a
gngsn <rudtjs4540@gmail.com>
2021-04-24 02:39:36 +0900

    update: fix config files

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
+echo " > 배포 시"
+
 APP_NAME=ruvve-server
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 echo "> CURRENT_PID => $CURRENT_PID"
+
 if [ -z $CURRENT_PID ]
 then
   echo "> 종료할 것 없음"
@@ -12,3 +15,5 @@ else
 fi
 
 nohup $JAVA_HOME/bin/java -jar ./build/libs/ruvve-server-0.0.1-SNAPSHOT.jar > ./nohup.out 2>&1 &
+
+echo " > 배포 완료"
